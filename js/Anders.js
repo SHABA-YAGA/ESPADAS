88cd7fdbd06399d17fc3e0c4ef2b83fa5cd9c233
opanga77 <andersoking77@gmail.com>
2020-03-03 17:44:54 +0300

    commit

---
+++ js/Anders.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myList li").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
